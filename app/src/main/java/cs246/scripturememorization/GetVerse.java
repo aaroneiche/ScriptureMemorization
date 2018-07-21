f9f1ae8982c1ca1d0be37acd3c66122c0dee8501
haydendsimmons <haydendsimmons@gmail.com>
2018-07-20 20:17:42 -0600

    Update GetVerse.java

--- app/src/main/java/cs246/scripturememorization/GetVerse.java
+++ app/src/main/java/cs246/scripturememorization/GetVerse.java
@@ -16,8 +16,14 @@ import android.widget.ListView;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/* GetVerse is a list activity. It will query the database for all of the verses in the previously
+selected chapter. All of those verses will fill a list that is then used to populate
+the list view. When a verse is clicked, it's value is added to the scripture path to be passed
+by intent and the user is advanced to the next selection phase.
+*/
 
 public class GetVerse extends ListActivity {
+    //immutable strings used to construct the queries and normal strings to be defined later for the intent
     private static final String TABLE_NAME = "verses";
     private static final String CHAPTER_SELECTOR = "chapter_id = ";
     private static final String VERSE_NUMBER = "verse_number";
@@ -31,6 +37,7 @@ public class GetVerse extends ListActivity {
     private static int VERSE_ID;
     private static String VERSE_TEXT;
 
+    //the list view, the verses arraylist, the verseIDs arraylist, and the verseTexts arraylist.
     private ListView listView;
     private ArrayList<String> verses;
     private ArrayList<Integer> verseIDs;
@@ -38,6 +45,7 @@ public class GetVerse extends ListActivity {
 
     private Scripture s;
 
+    //connect to the databse
     private DatabaseHelper mDBHelper;
     private SQLiteDatabase mDb;
 
@@ -69,17 +77,23 @@ public class GetVerse extends ListActivity {
         } catch (SQLException mSQLException) {
             throw mSQLException;
         }
+        /*fill the chapters arraylist and populate the list view with it.
+        Fill the verseIDs and verseTexts arraylists too. */
         fillVerses();
         fillVerseIDs();
         fillVerseTexts();
         setUpList();
     }
 
+    //Populate the list with the verses arraylist
     private void setUpList() {
         setListAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, verses));
         listView = getListView();
 
+        /*when a list view item is clicked, the volume title, volume id, the book id, the book title,
+        the chapter title, the chapter id, the verse name (number), the verse id, and the verse text
+        are all put into the intent and then we go back to the main activity!*/
         listView.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -88,7 +102,7 @@ public class GetVerse extends ListActivity {
                 VERSE_TEXT = verseTexts.get(position);
                 Log.d("The scripture path is", "" + VOLUME_TITLE + " " + BOOK_TITLE + " " + CHAPTER_TITLE + " " +  VERSE_TITLE + " and the verse ID is " + VERSE_ID + " and the verse text is " + VERSE_TEXT);
 
-                s = new Scripture(VOLUME_TITLE, BOOK_TITLE, CHAPTER_TITLE, VERSE_TITLE, VERSE_ID, VERSE_TEXT);
+                s = new Scripture(VOLUME_TITLE, BOOK_TITLE, CHAPTER_TITLE, VERSE_TITLE, VERSE_TEXT);
 
                 intent.putExtra("Scripture", s);
                 setResult(RESULT_OK, intent);
@@ -97,6 +111,7 @@ public class GetVerse extends ListActivity {
         });
     }
 
+    //the database is queried and will return all of the verse names (numbers). Each will be added to the arraylist.
     private void fillVerses() {
         verses = new ArrayList<String>();
         Cursor verseCursor = mDb.query(TABLE_NAME, new String[] {VERSE_NUMBER}, CHAPTER_SELECTOR + CHAPTER_ID, null, null, null, "id");
@@ -110,6 +125,7 @@ public class GetVerse extends ListActivity {
         verseCursor.close();
     }
 
+    //the database is queried and will return all of the verse ids. Each will be added to the arraylist.
     private void fillVerseIDs() {
         verseIDs = new ArrayList<Integer>();
         Cursor verseIDCursor = mDb.query(TABLE_NAME, new String[] {"id"}, CHAPTER_SELECTOR + CHAPTER_ID, null, null, null, "id");
@@ -123,6 +139,7 @@ public class GetVerse extends ListActivity {
         verseIDCursor.close();
     }
 
+    //the database is queried and will return all of the verse text values. Each will be added to the arraylist.
     private void fillVerseTexts() {
         verseTexts = new ArrayList<String>();
         Cursor verseTextCursor = mDb.query(TABLE_NAME, new String[] {"scripture_text"}, CHAPTER_SELECTOR + CHAPTER_ID, null, null, null, "id");
