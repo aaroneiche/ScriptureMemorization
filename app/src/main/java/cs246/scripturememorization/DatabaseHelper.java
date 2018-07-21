be080c4aa839dc9f8553ffe118fa52e2a8c90100
haydendsimmons <haydendsimmons@gmail.com>
2018-07-20 19:37:01 -0600

    Update DatabaseHelper.java

--- app/src/main/java/cs246/scripturememorization/DatabaseHelper.java
+++ app/src/main/java/cs246/scripturememorization/DatabaseHelper.java
@@ -11,15 +11,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/*
+This database helper class extends the SQLiteOpenHelper in order to provide the rest of 
+the app access to the SQLite database. It will be used in the GetVolume, GetBook, GetChapter,
+and GetVerse activities. The databse must be copied from the assets folder in order to be used.
+This class contains methods that can accomplish this.
+*/
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+    //the database name is defined
     private static String DB_NAME = "scriptures.db";
     private static String DB_PATH = "";
     private static final int DB_VERSION = 1;
 
+    //a SQLite database is created and context is established
     private SQLiteDatabase mDataBase;
     private final Context mContext;
     private boolean mNeedUpdate = false;
 
+    //A usable path on the device is found and the database is copied in
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
         if (android.os.Build.VERSION.SDK_INT >= 17)
@@ -33,6 +43,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.getReadableDatabase();
     }
 
+    //if the databsae needs to be updated, it can be
     public void updateDataBase() throws IOException {
         if (mNeedUpdate) {
             File dbFile = new File(DB_PATH + DB_NAME);
@@ -45,11 +56,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    //check and see if the database exists where you say it exists. Maybe you're lying.
     private boolean checkDataBase() {
         File dbFile = new File(DB_PATH + DB_NAME);
         return dbFile.exists();
     }
 
+    //Copies the database if it exists and is readable
     private void copyDataBase() {
         if (!checkDataBase()) {
             this.getReadableDatabase();
@@ -62,6 +75,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    //copies the database from the assets folder
     private void copyDBFile() throws IOException {
         InputStream mInput = mContext.getAssets().open(DB_NAME);
         //InputStream mInput = mContext.getResources().openRawResource(R.raw.info);
@@ -75,11 +89,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         mInput.close();
     }
 
+    //a new database will be created if the one you provided isn't acceptable or if it doesn't exist.
     public boolean openDataBase() throws SQLException {
         mDataBase = SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.CREATE_IF_NECESSARY);
         return mDataBase != null;
     }
 
+    //close the database with this
     @Override
     public synchronized void close() {
         if (mDataBase != null)
@@ -87,11 +103,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super.close();
     }
 
+    //when the databse is created, do something
     @Override
     public void onCreate(SQLiteDatabase db) {
 
     }
 
+    //the database version can be upgraded
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (newVersion > oldVersion)
